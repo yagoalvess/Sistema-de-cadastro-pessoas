4eadd6ecbcd1881e3baf5b4bf5c12038078e278c
unknown <yagoalves455@gmail.com>
2023-10-17 21:58:49 -0300

    subindo os arquivos

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Paginas
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+
+@app.route("/lista_produtos")
+def usuarios():
+    return render_template("lista_produtos.html",)
+
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
