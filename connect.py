8747639c9fb2394374873b8356c1ab05eeb01e52
unknown <yagoalves455@gmail.com>
2023-10-23 23:34:40 -0300

    CRUD|DELETE

--- connect.py
+++ connect.py
@@ -1,12 +1,17 @@
-from tinydb import TinyDB
+from tinydb import TinyDB,Query
 from model import Pessoa
 
 
 db = TinyDB("Pessoas.json")
+usuario = Query()
+
+
+
 
 #inserir no banco json os dados da pessoa
 def inserir(model: Pessoa):
     db.insert({"CPF":model.CPF,"Nome":model.nome, "DataNascimento":model.dataNascimento})
+    print("Pessoa registrada com sucesso")
 
 
 #mostrar o dados no banco na tela 
@@ -14,3 +19,11 @@ def mostrarTodos():
     todos = db.all()
     return todos
 
+
+# deletar a pessoa no banco de dados
+def deletarPessoa(cpf: int):
+    if db.search(usuario.CPF==cpf):
+        db.remove(usuario.CPF==cpf)
+        print("Usuario deletado com sucesso")
+    else:
+        print("Usuario não encontrado")
--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-from connect import inserir, mostrarTodos
+from connect import inserir, mostrarTodos, deletarPessoa
 from model import Pessoa
 
 
-#função para salvar dados no banco de dados
+
+#funções
 def main():
-    p = Pessoa(13216,"Lucas","15-05-2000")
+    p = Pessoa(5000,"Amanda","30-05-2000")
     inserir(p)
-main()
-
-
-#função para monstrar os dados salvados no banco
-def main():
-    p = Pessoa(2910312093821803912,"Jorge","30-05-2000")
-    #inserir(p)
     t = mostrarTodos()
     for c in t:
         print(c)
 main()
+
+
