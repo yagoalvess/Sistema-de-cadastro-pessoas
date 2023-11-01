a2b6b570d0876b34f5e42254ba6f5c869d518f48
yagoalvess <yagoalves455@gmail.com>
2023-10-31 21:45:33 -0300

    tela de cadastro para o banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from connect import *
 from model import Pessoa
+from flask.globals import request
+
 
 app = Flask(__name__)
 
@@ -10,6 +12,17 @@ app = Flask(__name__)
 def homepage():
     result = mostrarTodos()
     return render_template("homepage.html",result = result)
+
+
+# função
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    nome = request.form["nome"]
+    cpf = request.form["cpf"]
+    dataNascimento = request.form["dataNascimento"]
+    pessoa = Pessoa(cpf,nome,dataNascimento)
+    inserir(pessoa)
+    return redirect("/")
 
 
 @app.route("/contatos")
@@ -25,6 +38,10 @@ def usuarios():
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
 
 
 
