832a31753867725e5ad8fa6234e3f66c3d130296
Yago Alves <yagoalves455@gmail.com>
2023-11-07 20:38:12 -0300

    update

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, redirect
 from connect import *
 from model import Pessoa
 from flask.globals import request
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 
 
 app = Flask(__name__)
@@ -31,7 +35,21 @@ if __name__ == "__main__":
 
 
 
+# configuração para aws
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
 
 
