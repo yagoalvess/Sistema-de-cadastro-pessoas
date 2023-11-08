af9d26b2c3866aa9d26f94196581546d205af571
Yago Alves <yagoalves455@gmail.com>
2023-11-07 21:11:17 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,22 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
+ #configuração para aws
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
 
 app = Flask(__name__)
 
@@ -35,21 +51,6 @@ if __name__ == "__main__":
 
 
 
-# configuração para aws
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+#
 
 
